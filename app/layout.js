17647302189421092ff5cee5d8a8224be71ebcc1
veron-eng <veronsundaram13@gmail.com>
2025-04-12 18:39:16 +0100

    added analytics to website

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en" data-theme="cupcake" className="scroll-smooth">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
